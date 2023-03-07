50b1c54d4d8b9bacc8d911b83f1be356e43fe3db
Hari <harishsarma.v@gmail.com>
2023-03-07 21:45:39 +0530

    updating tf-module-subnets

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "aws_subnet" "main" {
   cidr_block        = var.cidr_block[count.index]
   availability_zone = var.availability_zone[count.index]
   vpc_id            = var.vpc_id
+
   tags = merge(
     local.common_tags,
     { Name = "${var.env}-${var.name}-subnet-${count.index+1}" }
