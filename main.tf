a062c000b8d807d46f81ce2d8da94887a5d3abd0
Hari <harishsarma.v@gmail.com>
2023-03-10 16:27:13 +0530

    updating tf-module-subnets

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_route_table" "route_table" {
   vpc_id = var.vpc_id
 
   route {
-    cidr_block = data.aws_vpc.default.cidr_block
+    cidr_block                = data.aws_vpc.default.cidr_block
     vpc_peering_connection_id = var.vpc_peering_connection_id
   }
 
